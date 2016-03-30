7880993491218923b0ea0e09702670b6f4f7daa5
ryan419 <ryan.shaw.0419@gmail.com>
2016-03-30 13:58:25 +1100

    added genmapxml.php

---
+++ genmapxml.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xiao
+ * Date: 29/03/2016
+ * Time: 4:24 PM
+ */
+require("dbinfo.php");
+
+function parseToXML($htmlStr)
+{
+    $xmlStr=str_replace('<','&lt;',$htmlStr);
+    $xmlStr=str_replace('>','&gt;',$xmlStr);
+    $xmlStr=str_replace('"','&quot;',$xmlStr);
+    $xmlStr=str_replace("'",'&#39;',$xmlStr);
+    $xmlStr=str_replace("&",'&amp;',$xmlStr);
+    return $xmlStr;
+}
+
+
+
+// Opens a connection to a MySQL server
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die($conn->connect_error);
+$query = 'SELECT E.name as ename, V.name as vname, E.start as start, E.end as end, E.logo as logo,
+ V.latitude as lat, V.longitude as lon, E.culture as culture FROM Events E ,Venues V WHERE E.Venue = V.id';
+$result = $conn->query($query);
+if (!$result) die($conn->error);
+
+header("Content-type: text/xml");
+//foreach ($result as $row){
+//    print_r($row);
+//}
+
+echo '<markers>';
+
+// Iterate through the rows, adding XML nodes for each
+foreach ($result as $row){
+    // ADD TO XML DOCUMENT NODE
+    echo '<marker ';
+    echo 'event="' . parseToXML(stripslashes($row['ename'])) . '" ';
+    echo 'venue="' . parseToXML(stripslashes($row['vname'])) . '" ';
+    echo 'start="' . parseToXML(stripslashes($row['start'])) . '" ';
+    echo 'end="' . parseToXML(stripslashes($row['end'])) . '" ';
+    echo 'logo="' . parseToXML(stripslashes($row['logo'])) . '" ';
+    echo 'lat="' . parseToXML(stripslashes($row['lat'])) . '" ';
+    echo 'lon="' . parseToXML(stripslashes($row['lon'])) . '" ';
+    echo 'type="' . parseToXML(stripslashes($row['culture'])) . '" ';
+    echo '/>';
+}
+echo '</markers>';
+
+
+
+?>
